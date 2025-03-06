e4489a8eefac43953778d07d28d4437916cedea3
Rafael Vicente <rafaelpv15@gmail.com>
2025-03-06 20:58:27 -0300

    fix: adicionando proxy para o geonames api

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,14 @@ import { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_APIKEY: process.env.NEXT_PUBLIC_APIKEY,
-    NEXT_PUBLIC_GEONAMEKEY: process.env.NEXT_PUBLIC_APIKEY,
+    NEXT_PUBLIC_GEONAMEKEY: process.env.NEXT_PUBLIC_GEONAMEKEY,
   },
+  rewrites: async () => [
+    {
+      source: '/api/geonames/:path*',
+      destination: 'http://api.geonames.org/:path*',
+    },
+  ],
 };
 
 export default nextConfig;
