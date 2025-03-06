a89c600db1d5ffae32f63afe6deb734b7bdc3af4
Rafael Vicente <rafaelpv15@gmail.com>
2025-03-06 20:57:09 -0300

    Revert "fix:  adicionando proxy para o geonames api"
    This reverts commit 5107fefc96b02c14b6ca568a16f4e6254f5cded1.

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,8 @@ import { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_APIKEY: process.env.NEXT_PUBLIC_APIKEY,
-    NEXT_PUBLIC_GEONAMEKEY: process.env.NEXT_PUBLIC_GEONAMEKEY,
+    NEXT_PUBLIC_GEONAMEKEY: process.env.NEXT_PUBLIC_APIKEY,
   },
-  rewrites: async () => [
-    {
-      source: '/api/geonames/:path*',
-      destination: 'http://api.geonames.org/:path*',
-    },
-  ],
 };
 
 export default nextConfig;
